d2f7159731e5735af635c6e29c1f0f3d57b941c2
jnm2 <me@jnm2.com>
2020-02-01 13:12:07 -0500

    Clean up GlobalSuppressions.cs

--- src/AmbientTasks/GlobalSuppressions.cs
+++ src/AmbientTasks/GlobalSuppressions.cs
@@ -1,6 +1,3 @@
-// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
+using System.Diagnostics.CodeAnalysis;
 
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "Out of scope", Scope = "module")]
+[assembly: SuppressMessage("Globalization", "CA1303:Do not pass literals as localized parameters", Justification = "Out of scope", Scope = "module")]
