2adb709eef6bf802672385d3b7f049bad38ab0ff
jnm2 <me@jnm2.com>
2021-01-10 11:38:33 -0500

    Move to GitHub Actions

--- build/CiServerIntegration.ps1
+++ build/CiServerIntegration.ps1
@@ -12,7 +12,18 @@ function Get-BuildMetadata {
         $metadata.PullRequestNumber = $env:System_PullRequest_PullRequestNumber
         $metadata.BranchName = $env:Build_SourceBranchName
     }
-    elseif ($env.CI) {
+    elseif ($env:GITHUB_ACTIONS) {
+        $metadata.BuildNumber = $env:GITHUB_RUN_NUMBER
+
+        if ($env:GITHUB_REF.StartsWith('refs/pull/')) {
+            $trimmedRef = $env:GITHUB_REF.Substring('refs/pull/'.Length)
+            $metadata.PullRequestNumber = $trimmedRef.Substring(0, $trimmedRef.IndexOf('/'))
+            $metadata.BranchName = $env:GITHUB_BASE_REF
+        } elseif ($env:GITHUB_REF.StartsWith('refs/heads/')) {
+            $metadata.BranchName = $env:GITHUB_REF.Substring('refs/heads/'.Length)
+        }
+    }
+    elseif ($env:CI) {
         throw 'Build metadata detection is not implemented for this CI server.'
     }
 
@@ -23,7 +34,10 @@ function Update-CiServerBuildName([Parameter(Mandatory=$true)] [string] $BuildNa
     if ($env:TF_BUILD) {
         Write-Output "##vso[build.updatebuildnumber]$BuildName"
     }
-    elseif ($env.CI) {
+    elseif ($env:GITHUB_ACTIONS) {
+        # GitHub Actions does not appear to have a way to dynamically update the name/number of a workflow run.
+    }
+    elseif ($env:CI) {
         throw 'Build name updating is not implemented for this CI server.'
     }
 }
